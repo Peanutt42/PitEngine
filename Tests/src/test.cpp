56502d7d7c5a069b12d7578abaa84f1e69513760
Peanutt42 <97802125+Peanutt42@users.noreply.github.com>
2022-11-16 21:26:12 +0100

    Removed Optick testing from Tests

--- Tests/src/test.cpp
+++ Tests/src/test.cpp
@@ -1,25 +1,6 @@
 #include <PitTest.hpp>
-#include <thread>
-#include "C:\dev\cpp\PitEngine\vendor\optick\include\optick.h"
 
-static void Thread() {
-	OPTICK_THREAD("TestThread");
-
-	while (true) {
-		OPTICK_EVENT("TestThread::Update");
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
-
-	}
-
-}
 
 PIT_TEST(SomeTestGroup,
-	while (true) {
-		OPTICK_FRAME("Test");
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-		for (int i = 0; i < 10; i++) {
-			std::thread tread(Thread);
-			tread.detach();
-		}
-	}
+	
 );
