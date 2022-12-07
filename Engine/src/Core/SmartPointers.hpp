9d90a43859f4257389e286cb4e6251cd848d4707
Peanutt42 <97802125+Peanutt42@users.noreply.github.com>
2022-12-07 17:40:09 +0100

    Added more Smartpointer Features + better Tests

--- Engine/src/Core/SmartPointers.hpp
+++ Engine/src/Core/SmartPointers.hpp
@@ -30,7 +30,7 @@ namespace Pit {
 			dyingObj.m_Ptr = nullptr;
 		}
 
-		explicit operator bool() const { return m_Ptr; }
+		explicit operator const bool() const { return m_Ptr; }
 
 		T* operator->() {
 			if (!m_Ptr)
@@ -48,71 +48,20 @@ namespace Pit {
 			if (!m_Ptr)
 				std::cerr << "[Engine::General]    Pointer was accessed but was nullptr!\n";
 			return m_Ptr;
+		}
+
+		void Release() {
+			_Cleanup();
 		}
 
 	private:
 		T* m_Ptr;
 
 		inline void _Cleanup() {
-			if (m_Ptr) delete m_Ptr;
-		}
-	};
-
-	// NativeArray specific implomentation
-	template<class T>
-	class ScopeRef<T[]> {
-	public:
-		ScopeRef(T* ptr = nullptr) : m_Ptr(ptr) {}
-
-		ScopeRef(const ScopeRef<T>& obj) = delete;
-		ScopeRef& operator=(const ScopeRef<T>& obj) = delete;
-
-		ScopeRef(ScopeRef<T>&& dyingObj) {
-			_Cleanup();
-
-			m_Ptr = dyingObj.m_Ptr;
-			dyingObj.m_Ptr = nullptr;
-		}
-
-		~ScopeRef() {
-			_Cleanup();
-		}
-
-		void operator=(ScopeRef<T>&& dyingObj) {
-			_Cleanup();
-
-			m_Ptr = dyingObj.m_Ptr;
-			dyingObj.m_Ptr = nullptr;
-		}
-
-		explicit operator bool() const { return m_Ptr; }
-
-		T* operator->() {
-			return m_Ptr;
-		}
-
-		T& operator*() {
-			return *m_Ptr;
-		}
-
-		T* Get() {
-			if (!m_Ptr)
-				std::cerr << "[Engine::General]    Pointer was accessed but was nullptr!\n";
-			return m_Ptr;
-		}
-
-		T& operator[](int index) {
-			if (index < 0)
-				throw std::runtime_error("Negative index exception");
-			
-			return m_Ptr[index];
-		}
-
-	private:
-		T* m_Ptr = nullptr;
-
-		inline void _Cleanup() {
-			if (m_Ptr) delete[] m_Ptr;
+			if (m_Ptr) {
+				delete m_Ptr;
+				m_Ptr = nullptr;
+			}
 		}
 	};
 
@@ -156,7 +105,7 @@ namespace Pit {
 			return *this;
 		}
 
-		explicit operator bool() const { return m_Ptr; }
+		explicit operator const bool() const { return m_Ptr && m_References; }
 
 		T* operator->() {
 			if (!m_Ptr)
@@ -176,7 +125,11 @@ namespace Pit {
 			return m_Ptr;
 		}
 
-		unsigned short GetReferences() {
+		void Release() {
+			_Cleanup();
+		}
+
+		const unsigned short GetReferences() const {
 			if (m_References) return m_References->GetCount();
 			
 			return 0;
@@ -188,77 +141,14 @@ namespace Pit {
 		ReferenceCounter* m_References;
 
 		inline void _Cleanup() {
-			if (m_Ptr) delete m_Ptr;
-			if (m_References) delete m_References;
-		}
-	};
-
-	template<class T>
-	class Ref<T[]> {
-	public:
-		Ref(T* ptr = nullptr) : m_Ptr(ptr) {
-
-			m_References = new ReferenceCounter(1);
-		}
-
-		Ref(const Ref<T[]>& obj) : m_Ptr(obj.m_Ptr), m_References(obj.m_References) {
-			obj.m_References->AddRef();
-		}
-
-		~Ref() {
-			if (m_References->Release() == 0)
-				_Cleanup();
-		}
-
-		Ref<T[]>& operator=(const Ref<T[]>& other) {
-			if (this != &other) {
-				if (m_References->Release() == 0)
-					_Cleanup();
-
-				m_Ptr = other.m_Ptr;
-				m_References = other.m_References;
-				m_References->AddRef();
+			if (m_Ptr) {
+				delete m_Ptr;
+				m_Ptr = nullptr;
 			}
-			return *this;
-		}
-
-		explicit operator bool() const { return m_Ptr; }
-
-		T* operator->() {
-			if (!m_Ptr)
-				std::cerr << "[Engine::General]    Pointer was accessed but was nullptr!\n";
-			return m_Ptr;
-		}
-
-		T& operator*() {
-			if (!m_Ptr)
-				std::cerr << "[Engine::General]    Pointer was accessed but was nullptr!\n";
-			return *m_Ptr;
-		}
-
-		T* Get() {
-			if (!m_Ptr)
-				std::cerr << "[Engine::General]    Pointer was accessed but was nullptr!\n";
-			return m_Ptr;
-		}
-
-		T& operator[](int index) {
-			if (index < 0)
-				throw std::runtime_error("Negative index exception");
-			size_t arraySize = *(&m_Ptr + 1) - m_Ptr;
-			if (index >= arraySize)
-				throw std::runtime_error("Array index out of bounds!");
-
-			return m_Ptr[index];
-		}
-
-	private:
-		T* m_Ptr;
-		ReferenceCounter* m_References;
-
-		inline void _Cleanup() {
-			if (m_Ptr) delete m_Ptr;
-			if (m_References) delete m_References;
+			if (m_References) {
+				delete m_References;
+				m_References = nullptr;
+			}
 		}
 	};
 }
